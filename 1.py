864c4da745cbdcfd7cb3ed064d8c7131ac55c534
cheats2 <nikitavorobtj459@gmail.com>
2025-09-01 19:25:43 +0300

    my first commit

---
+++ 1.py
@@ -0,0 +1,6 @@
+password=12345678
+password1=int(input("ведіть пароли: "))
+if password1==password:
+    print("вхід дозволений")
+else:
+    print("вхід заборонений")
---
+++ 5.py
@@ -0,0 +1,6 @@
+name = input("введіть своє ім'я: ")
+city = input("Введіть своє місто: ")
+
+hobby = input("Яке у вас хобі? ")
+
+print(f"Ви {name} з {city} і ваше хобі — {hobby}")
