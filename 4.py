95358f9063ccf85af9a1bfcd6ef4316b8c2d4126
cheats2 <nikitavorobtj459@gmail.com>
2025-09-01 19:25:43 +0300

    my first commit

---
+++ 4.py
@@ -0,0 +1,32 @@
+a = int(input("Введіть перше число: "))
+
+b = int(input("Введіть друге число: "))
+
+result = a * b
+
+print("Добуток:", result)
+
+
+a = int(input("Введіть перше число: "))
+
+b = int(input("Введіть друге число: "))
+
+result = a + b
+
+print("Сума:", result)
+
+a = int(input("Введіть перше число: "))
+
+b = int(input("Введіть друге число: "))
+
+result = a - b
+
+print("Різниця:", result)
+
+a = int(input("Введіть перше число: "))
+
+b = int(input("Введіть друге число: "))
+
+result = a / b
+
+print("Частка:", result)
